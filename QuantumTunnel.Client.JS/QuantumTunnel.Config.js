617d8893f610962a672e4a4e40d0eaf4c99385b1
sharpcodex <sharpcodex@gmail.com>
2016-05-13 19:11:46 +0300

    add html config setters

--- QuantumTunnel.Client.JS/QuantumTunnel.Config.js
+++ QuantumTunnel.Client.JS/QuantumTunnel.Config.js
@@ -1,12 +1,47 @@
 ﻿var QT = (function (qt) {
 
+    //#region init
+
+    var thisScript = $("script[quantum-tunnel]");
+
+    //#endregion
+
+    //#region helpers
+
+    function getCamelCase(str) {
+        return str.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); });
+    }
+
+    function parseAttribute(value) {
+        if (!value) return null;
+        if (value === "true" || value === "false") return /^true$/i.test(value);
+        if (!isNaN(value)) return parseFloat(value);
+        return value;
+    }
+
+    //#end
+
+    //#region default config
+
     qt.config = {
-        log: {
-            connectionEvents: true,
-            internalEvents: true,
-            tunnelingEvents:true
-        }
+        logConnectionEvents: false,
+        logInternalEvents: true,
+        logBroadcastingEvents: true,
+        logListeningEvents: true,
+
+        init: true
     };
 
+    //#end
+
+    //#region config setters
+
+    $.each(thisScript.get(0).attributes, function (i, attrib) {
+        qt.config[getCamelCase(attrib.name)] = parseAttribute(attrib.value);
+    });
+
+    //#end
+
+    console.log(qt.config);
     return qt;
 }(QT || {}));
