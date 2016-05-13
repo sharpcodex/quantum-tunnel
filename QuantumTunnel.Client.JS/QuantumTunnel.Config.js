05bbb764cb772ce70d5d79a87e05952a32e04e8c
sharpcodex <sharpcodex@gmail.com>
2016-05-14 01:24:05 +0300

    add loggers

--- QuantumTunnel.Client.JS/QuantumTunnel.Config.js
+++ QuantumTunnel.Client.JS/QuantumTunnel.Config.js
@@ -2,11 +2,11 @@
 
     //#region init
 
-    var thisScript = $("script[quantum-tunnel]");
+    var qTag = $("script[quantum-tunnel]");
 
     //#endregion
 
-    //#region helpers
+    //#region private helpers
 
     function getCamelCase(str) {
         return str.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); });
@@ -19,12 +19,53 @@
         return value;
     }
 
-    //#end
+    function printMessage(msg) {
+        if (console && console.log) {
+            console.log("QT : ", msg);
+        } else {
+            alert("QT info : ", msg);
+        }
+    };
+
+    function printError(err) {
+        if (console && console.error()) {
+            console.error("QT : ", err);
+        } else {
+            alert("QT error : ", err);
+        }
+    };
+
+    //#endregion
+
+    //#region public helpers
+
+    qt.logConnection = function (msg) {
+        if (qt.config.logConnectionEvents) printMessage(msg);
+    };
+
+    qt.logInternal = function (msg) {
+        if (qt.config.logInternalEvents) printMessage(msg);
+    };
+
+    qt.logBroadcasting = function (msg) {
+        if (qt.config.logBroadcastingEvents) printMessage(msg);
+    };
+
+    qt.logListening = function (msg) {
+        if (qt.config.logListeningEvents) printMessage(msg);
+    };
+
+    qt.logError = function (err) {
+        if (qt.config.logErrors) printError(err);
+    };
+
+    //#endregion
 
     //#region default config
 
     qt.config = {
-        logConnectionEvents: false,
+        logErrors: true,
+        logConnectionEvents: true,
         logInternalEvents: true,
         logBroadcastingEvents: true,
         logListeningEvents: true,
@@ -32,16 +73,22 @@
         init: true
     };
 
-    //#end
+    //#endregion
 
     //#region config setters
 
-    $.each(thisScript.get(0).attributes, function (i, attrib) {
-        qt.config[getCamelCase(attrib.name)] = parseAttribute(attrib.value);
+    $.each(qTag.get(0).attributes, function (i, attrib) {
+        try {
+            qt.config[getCamelCase(attrib.name)] = parseAttribute(attrib.value);
+        } catch (e) {
+            qt.logError("setting attributes configs" + e);
+        }
     });
 
-    //#end
+    //#endregion
 
-    console.log(qt.config);
+    qt.logInternal("QT initialized!");
+    qt.logInternal(qt.config);
+
     return qt;
 }(QT || {}));
