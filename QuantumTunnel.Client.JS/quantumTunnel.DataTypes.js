c8d2ee91c0bc43b002e03cb4cc9315393f79ebeb
sharpcodex <sharpcodex@gmail.com>
2016-05-24 17:48:51 +0300

    Add data types module

---
+++ QuantumTunnel.Client.JS/quantumTunnel.DataTypes.js
@@ -0,0 +1,23 @@
+﻿//================================================
+// quantumTunnel.Broadcasters
+// sharpcodex , sharpcodex@gmail.com
+//================================================
+
+var QT = (function (qt) {
+    "use strict";
+    var types = {}
+
+    //#region quantumTunnel.Broadcasters
+
+    types.broadcastingClients = {
+        self: "self",
+        others: "others",
+        group: "group",
+        all: "all"
+    }
+
+    //#endregion
+
+    qt.dataTypes = types;
+    return qt;
+}(QT || {}));
